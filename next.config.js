8b036ed9f11ac27c633c7c127fd47c2a8fcabc5b
tatta-chotdog <tatta875dev@gmail.com>
2025-03-29 09:18:29 +0900

    fix: csp

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://*.supabase.co https://supabase.co;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' blob: https://accounts.google.com https://apis.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://*.googleusercontent.com; font-src 'self'; connect-src 'self' https://*.supabase.co https://supabase.co https://accounts.google.com https://apis.google.com;",
           },
         ],
       },
